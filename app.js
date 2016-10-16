72f137bf77ebeccb0af8d05938d92e724950ac57
Gary <F74044038@mail.ncku.edu.tw>
2016-10-16 22:44:11 +0800

    animate

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-jQuery('.welcome').animate({'margin-left':'3000px'},2000);
+jQuery('.welcome').animate({'margin-left':'1750px'},8000);
+
+$('#schooll').click(function(){
+$('#schooll').hide();
+$('#school').show();
+  });
+
+  $('#colorr').click(function(){
+  $('#colorr').hide();
+  $('#color').show();
+    });
+
+    $('#foodd').click(function(){
+    $('#foodd').hide();
+    $('#food').show();
+      });
+
+      $('#hobbyy').click(function(){
+      $('#hobbyy').hide();
+      $('#hobby').show();
+        });
+
+        $('#maill').click(function(){
+        $('#maill').hide();
+        $('#mail').show();
+          });
