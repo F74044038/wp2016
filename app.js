8fb664d8f2d1c6c94a3bad3c6ac73c47d5bfd5f9
Gary <F74044038@mail.ncku.edu.tw>
2016-10-16 23:40:34 +0800

    osdsdf

--- app.js
+++ app.js
@@ -4,25 +4,25 @@ $(document).ready(function(){
   $("#school").show();
   });
           
-/*  $('#colorr').click(function(){
-  $('#colorr').hide();
-  $('#color').show();
+  $("#colorr").click(function(){
+  $("#colorr").hide();
+  $("#color").show();
   });
                     
-  $('#foodd').click(function(){
-  $('#foodd').hide();
-  $('#food').show();
+  $("#foodd").click(function(){
+  $("#foodd").hide();
+  $("#food").show();
   });
 
-  $('#hobbyy').click(function(){
-  $('#hobbyy').hide();
-  $('#hobby').show();
+  $("#hobbyy").click(function(){
+  $("#hobbyy").hide();
+  $("#hobby").show();
   });
 
-  $('#maill').click(function(){
-  $('#maill').hide();
-  $('#mail').show();
-  });*/
+  $("#maill").click(function(){
+  $("#maill").hide();
+  $("#mail").show();
+  });
                                               
    });
 
