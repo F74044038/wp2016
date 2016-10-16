a7be1cfbd8ced695465ebdb3805481766316c3fe
Gary <F74044038@mail.ncku.edu.tw>
2016-10-16 23:23:00 +0800

    iasfsd

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
-jQuery('.welcome').animate({'margin-left':'1750px'},8000);
-
-$('#schooll').click(function(){
-$('#schooll').hide();
-$('#school').show();
-  });
-
-  $('#colorr').click(function(){
-  $('#colorr').hide();
-  $('#color').show();
-    });
-
-    $('#foodd').click(function(){
-    $('#foodd').hide();
-    $('#food').show();
-      });
-
-      $('#hobbyy').click(function(){
-      $('#hobbyy').hide();
-      $('#hobby').show();
+$(document).ready(function(){
+  $('#schooll').click(function(){
+    $('#schooll').hide();
+      $('#school').show();
         });
+          
+            $('#colorr').click(function(){
+              $('#colorr').hide();
+                $('#color').show();
+                  });
+                    
+                      $('#foodd').click(function(){
+                        $('#foodd').hide();
+                          $('#food').show();
+                            });
 
-        $('#maill').click(function(){
-        $('#maill').hide();
-        $('#mail').show();
-          });
+                              $('#hobbyy').click(function(){
+                                $('#hobbyy').hide();
+                                  $('#hobby').show();
+                                    });
+
+                                      $('#maill').click(function(){
+                                        $('#maill').hide();
+                                          $('#mail').show();
+                                            });
+                                              
+                                              });
+
