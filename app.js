a1aec98f1486775145cd44190d03f84ad80e5eb8
Gary <F74044038@mail.ncku.edu.tw>
2016-10-16 23:43:06 +0800

    sdfsdf

--- app.js
+++ app.js
@@ -3,26 +3,33 @@ $(document).ready(function(){
   $("#schooll").hide();
   $("#school").show();
   });
-          
+});
+
+$(document).ready(function(){
   $("#colorr").click(function(){
   $("#colorr").hide();
   $("#color").show();
   });
-                    
+});
+
+$(document).ready(function(){
   $("#foodd").click(function(){
   $("#foodd").hide();
   $("#food").show();
   });
+});
 
+$(document).ready(function(){
   $("#hobbyy").click(function(){
   $("#hobbyy").hide();
   $("#hobby").show();
   });
+});
 
+$(document).ready(function(){
   $("#maill").click(function(){
   $("#maill").hide();
   $("#mail").show();
-  });
-                                              
-   });
+  });                                              
+});
 
